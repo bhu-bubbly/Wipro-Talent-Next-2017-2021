849b56c14910f53fb68dc1124ca19a0b3591ccbf
bhu-bubbly <64955411+bhu-bubbly@users.noreply.github.com>
2020-05-27 20:43:05 +0530

    Add files via upload

---
+++ DAY_1_PROGRAMS.java
@@ -0,0 +1,278 @@
+
+   1. PROGRAM 
+
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    
+         int number = 100;
+         if(number >= 0)
+         {
+          
+             system.out.println( number+ " is a  positive number ");
+         }
+         
+          else 
+          {
+             system.out.println( number+ " is a  negative number ");
+         
+          }    
+	}     
+	
+}
+
+=====================================================
+   2. PROGRAM 
+
+   import java.util.*;
+   public class Main
+      {
+        public static void main(String[] args)
+     {
+        scanner sc = new scanner (system.in);
+         int number1,number2;
+         number1 = sc.nextInt();
+         number2 = sc.nextInt();
+         if (number1 > number2)
+         {
+            system.out.println(number1 "is greater");
+         }
+         else 
+         {
+            system.out.println(number2 "is greater");
+         }    
+     }	    
+	
+    }
+=========================================================
+
+    3.PROGRAM
+
+    import java.util.scanner;
+    public class main
+   {
+       public static void main(String[] args) 
+       {
+         scanner sc = new scanner (system.in);
+         syatem.out.print("Enter a number : ");
+         if (number % 2==0)
+         {
+            system.out.println(number "is even");
+         }
+         else 
+         {
+            system.out.println(number2 "is odd");
+         }    
+      }
+   }
+=============================================================	   
+
+   4.PROGRAM
+
+   import java.util.scanner;
+   public class main
+  {
+	public static void main(String[] args) 
+	{
+	     Int num1,num2 sum
+	     scanner sc = new scanner (system.in);
+	     syatem.out.print("Enter num1 : ");
+                      syatem.out.print("Enter num2 : ");
+                     sum = num1+num2;
+                     if (sum % 2==0)
+                     {
+                      system.out.println(sum "is even");
+                     }
+                   else 
+                     {
+                      system.out.println(sum "is odd");
+                     }    
+	}     
+	
+  }
+===================================================
+
+    5. PROGRAM
+
+    import java.util.*;
+    public class main
+ {
+   static void printnumbers (int n)
+   {
+       if (n >0)
+       {
+           printnumbers (n - 1);
+           system.out.print(n + " ");
+           
+       }
+       return;
+   }
+   public static void main (string[]) args)
+   {
+       printnumbers(100);
+   }
+	
+ }
+==================================================
+   
+    6.PROGRAM
+   
+   import java.util.*;
+   public class main
+  {
+     public static void main(strings[] args)
+    {
+      for (int i = 1; i < 100; i++) 
+      {
+         if (i % 2 != 0)
+         {
+             system.ou.println(i);
+         }
+      }
+    
+    }
+	
+ }
+======================================================
+
+    7.PROGRAM
+
+     import java.util.*;
+     class java
+   {
+     public static void main(strings[] args)
+     {
+       int n = 100;
+       system.out.print(" alternate numbers from 0 to "+n+" are: ");
+       for (int i = 0; i<=n; i++)
+       {
+          if (i % 2 == 0)
+           {
+              system.out.print(i + " ");
+           }
+       }
+    
+      }
+	
+   }
+=========================================================
+
+    8.PROGRAM
+
+
+     import java.util.*;
+    public class main
+  {
+      static void printnumbers (int n)
+     {
+        if (n >=0)
+         {
+           printnumbers (n);
+           system.out.print(n - " ");
+           
+         }
+       return;
+     }
+        public static void main (string[]) args)
+      {
+          printnumbers(100);
+      }
+	
+   }
+=============================================================
+
+   
+
+    
+ 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
